ecdaa0496d93ac58b34150b208d17bdc1d725ab4
adrianrevilla009 <adrianrg1996@gmail.com>
2022-11-24 18:24:25 +0100

    create proyect

---
+++ src/test/java/masterCloudApps/web/bookReviews/BookReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package masterCloudApps.web.bookReviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
